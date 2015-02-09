99ba2743a280c08dd0b480d8b1a1952ea1451fae
Andreas Selvik <selvik@mit.edu>
2015-02-09 00:02:11 -0500

    Add comments

--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -28,23 +28,31 @@ func (mr *MapReduce) KillWorkers() *list.List {
 	return l
 }
 
+// Makes sure a specific job is executed and completed by at least one worker.
+
 func DoAsyncJob(mr *MapReduce, args DoJobArgs, wg *sync.WaitGroup){
     returnArgs := DoJobReply{OK: false}
-    workerID := ""
-    for ; !returnArgs.OK; {
+    var workerID string
+
+    for ; !returnArgs.OK ; {
+        //Wait for a free worker
         workerID = <-mr.registerChannel
+
         call(workerID,"Worker.DoJob", args, &returnArgs)
         if !returnArgs.OK{
             fmt.Printf("Async job failed!");
         }
     }
+    //Notify the workgroup and let the worker do more work!
     wg.Done()
     mr.registerChannel <- workerID
 }
 
 func (mr *MapReduce) RunMaster() *list.List {
+    //Waitgroup so we can syncronize after map and reduce
     var wg sync.WaitGroup
 
+    //Start all Map jobs
     for i:=0 ; i<mr.nMap;i++ {
         args := DoJobArgs{File:mr.file,
                         Operation:Map,
@@ -54,8 +62,10 @@ func (mr *MapReduce) RunMaster() *list.List {
         go DoAsyncJob(mr, args, &wg)
     }
 
+    //Wait for all map jobs to complete
     wg.Wait();
 
+    //Start all Reduce jobs
     for i:=0 ; i<mr.nReduce;i++ {
         args := DoJobArgs{File:mr.file,
                         Operation:Reduce,
@@ -64,6 +74,9 @@ func (mr *MapReduce) RunMaster() *list.List {
         wg.Add(1);
         go DoAsyncJob(mr, args, &wg)
     }
+
+    //Wait for all Reduce jobs
     wg.Wait();
+
 	return mr.KillWorkers()
 }
